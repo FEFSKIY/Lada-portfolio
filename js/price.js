7d558ed8caacc2ece3ce131fc40e7f2cab572337
FEFSKIY <111797731+FEFSKIY@users.noreply.github.com>
2022-09-08 01:29:27 +0300

    Add files via upload

---
+++ js/price.js
@@ -0,0 +1,259 @@
+"use strict"
+const isMobile = {
+   Android: function () {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+   },
+   Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: function () {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.iOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows());
+   }
+};
+// Устанвливаем класс touch устройствам с тачскрином, pc - без
+if (isMobile.any()) {
+   document.body.classList.add('_touch');
+
+   let menuArrows = document.querySelectorAll('.menu__arrow');
+   if (menuArrows.length > 0) {
+      for (let index = 0; index < menuArrows.length; index++) {
+         const menuArrow = menuArrows[index];
+         menuArrow.addEventListener("click", function (e) {
+            menuArrow.parentElement.classList.toggle('_active');
+
+         });
+      }
+   }
+
+} else {
+   document.body.classList.add('_pc');
+}
+
+//Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.header__menu-list');
+if (iconMenu) {
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
+
+// Плавная прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", onMenuLinkClick);
+   });
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+         if (iconMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            iconMenu.classList.remove('_active');
+            menuBody.classList.remove('_active');
+         }
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+      }
+   }
+}
+// Появление элементов при прокрутке
+// Шапка сайта
+window.addEventListener('scroll', () => {
+   let scrollDistance = window.scrollY;
+
+   const header = document.querySelector('.header');
+   if (scrollDistance >= 500) {
+      header.classList.add('scrolled');
+   } else {
+      header.classList.remove('scrolled');
+   }
+});
+
+// PopUP
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+// Время срабатывания анимации должно быть равно таймауту скрипта
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+   for (let index = 0; index < popupLinks.length; index++) {
+      const popupLink = popupLinks[index];
+      popupLink.addEventListener("click", function (e) {
+         const popupName = popupLink.getAttribute('href').replace('#', "");
+         const currentPopup = document.getElementById(popupName);
+         popupOpen(currentPopup);
+         e.preventDefault();
+      });
+   }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+   for (let index = 0; index < popupCloseIcon.length; index++) {
+      const el = popupCloseIcon[index];
+      el.addEventListener('click', function (e) {
+         popupClose(el.closest('.popup'));
+         e.preventDefault();
+      });
+   }
+}
+
+function popupOpen(currentPopup) {
+   if (currentPopup && unlock) {
+      const popupActive = document.querySelector('.popup.open');
+      if (popupActive) {
+         popupClose(popupActive, false);
+      } else {
+         bodyLock();
+      }
+      currentPopup.classList.add('open');
+      currentPopup.addEventListener("click", function (e) {
+         if (!e.target.closest('.popup__content')) {
+            popupClose(e.target.closest('.popup'));
+         }
+      });
+   }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+   if (unlock) {
+      popupActive.classList.remove('open');
+      if (doUnlock) {
+         bodyUnLock();
+      }
+   }
+}
+
+function bodyLock() {
+   const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+
+   if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+         const el = lockPadding[index];
+         el.style.paddingRight = lockPaddingValue;
+      }
+   }
+   body.style.paddingRight = lockPaddingValue;
+   body.classList.add('lock');
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+function bodyUnLock() {
+   setTimeout(function () {
+      if (lockPadding.length > 0) {
+         for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = '0px';
+         }
+      }
+      body.style.paddingRight = '0px';
+      body.classList.remove('lock');
+   }, timeout);
+
+   unlock = false;
+   setTimeout(function () {
+      unlock = true;
+   }, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+   if (e.which === 27) {
+      const popupActive = document.querySelector('.popup.open');
+      popupClose(popupActive);
+   }
+});
+
+(function () {
+   // проверяем поддержку
+   if (!Element.prototype.closest) {
+      // реализуем
+      Element.prototype.closest = function (css) {
+         var node = this;
+         while (node) {
+            if (node.matches(css)) return node;
+            else node = node.parentElement;
+         }
+         return null;
+      };
+   }
+})();
+(function () {
+   //проверяем поддержку
+   if (!Element.prototype.matches) {
+      //определяем свойство
+      Element.prototype.matches = Element.prototype.matchSelector ||
+         Element.prototype.webkitMatchesSelector ||
+         Element.prototype.mozMatchesSelector ||
+         Element.prototype.msMatchesSelector;
+   }
+})();
+
+// Плавное появление элементов при прокрутке
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll);
+   function animOnScroll(params) {
+      for (let index = 0; index < animItems.length; index++) {
+         const animItem = animItems[index];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+
+         }
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('_anim-active');
+         } else {
+            if (!animItem.classList.contains('_anim-no-hide')) {
+               animItem.classList.remove('_anim-active');
+            }
+         }
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+   }
+   setTimeout(() => {
+      animOnScroll();
+   }, 400);
+}
